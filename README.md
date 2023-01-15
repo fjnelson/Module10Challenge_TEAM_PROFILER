8fe7893b5ead2750dc4f5c5545cc740ed439d5f7
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-01-14 17:00:42 -0800

    initial code

---
+++ README.md
@@ -0,0 +1,31 @@
+# Module10Challenge_TEAM_Profiler
+
+## Description
+
+Bootcamp: Module 10 Challenge <br />
+Object-Oriented Programming Challenge: Team Profile Generator <br />
+Codebase correction that follows accessibility standards so that the site is optimized for search engines
+Uses Inquirer@8.2.4 - https://www.npmjs.com/package/inquirer/v/8.2.4
+
+## Table of Contents (Optional)
+
+N/A
+
+## Installation
+
+open integrated terminal for index.js and run 'npm i'
+
+## Usage
+Screenshot of readme creation
+![alt text](placeholder)
+
+Video of readme creation
+[Video Demo](placeholder)
+
+## Credits
+
+
+
+## License
+
+Please refer to the LICENSE in the repo.
---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// Your challenge is to build a Node.js command-line application that takes in information about employees on a software engineering team and generates an HTML webpage that displays summaries for each person.
+
+// Because this Challenge will require the use of the Inquirer package, ensure that you install and use Inquirer version 8.2.4. To do so, use the following command in your project folder: npm i inquirer@8.2.4.
+
+// Testing is key to making code maintainable, you’ll also write unit tests for each part of your code and ensure that it passes all of them.
+
+// This application won’t be deployed, you’ll also need to provide a link to a walkthrough video that demonstrates its functionality and all of the tests passing. You’ll need to submit a link to the video and add it to the README of your project.
