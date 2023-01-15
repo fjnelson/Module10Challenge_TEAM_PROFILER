6ddca9b2680f75153569e53fa15cdcd0938bcba1
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-01-15 11:03:20 -0800

    added engineer and intern questions

--- Develop/index.js
+++ Develop/index.js
@@ -17,7 +17,7 @@ const managerQuestions = [
             if (name !== "") {
                 return true;
             }
-            return "Enter in a name.";
+            return "Enter a name.";
         }
         },
         {
@@ -28,7 +28,7 @@ const managerQuestions = [
                 if (name !== "") {
                     return true;
                 }
-                return "Enter in an ID.";
+                return "Enter an ID.";
             }
           },
           {
@@ -39,7 +39,7 @@ const managerQuestions = [
                 if (name !== "") {
                     return true;
                 }
-                return "Enter in an email.";
+                return "Enter an email.";
             }
           },
           {
@@ -50,11 +50,150 @@ const managerQuestions = [
                 if (name !== "") {
                     return true;
                 }
-                return "Enter in a office number.";
+                return "Enter a office number.";
             }
           }
-    ];
+        ].then(answers => {
+            const newManager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOffice);
+            teamArr.push(newManager);
+            idArr.push(answers.managerId);
+            addTeamMember();
+        });
 
+function addTeamMember() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Add a team member.",
+            name: "choice",
+            choices: [
+                "Engineer",
+                "Intern",
+                "No other members"
+            ]
+        }
+    ]).then(userChoice => {
+        switch (userChoice.choice) {
+            case "Engineer":
+                engineerMember();
+                break;
+            case "Intern":
+                internMember();
+                break;
+            default:
+                generateHTML();
+        }
+    });
+}
+
+function engineerMember() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "What's the engineer's name?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Enter a name.";
+            }
+        },
+        {
+            type: "input",
+            name: "engineerId",
+            message: "What's the engineer's id?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Enter an ID.";
+            }
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What's the engineer's email?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Enter an email.";
+            }
+        },
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "What's the engineer's GitHub username?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Enter a username.";
+            }
+        }
+    ]).then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+        teamArr.push(engineer);
+        idArr.push(answers.engineerId);
+        addTeam();
+    });
+}
+
+function internMember() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "What's the intern's name?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Enter a name.";;
+            }
+        },
+        {
+            type: "input",
+            name: "internId",
+            message: "What's the intern's id?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Enter an ID.";
+            }
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What's the intern's email?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Enter an email.";
+            }
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "What's the intern's school?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Enter a school.";
+            }
+        }
+
+    ]).then(answers => {
+        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+        teamArr.push(intern);
+        idArr.push(answers.internId);
+        addTeam();
+    });
+}
 //     function writeToFile(teamData, data) {
 //         fs.writeFile(teamData, data, function (err) {
 //           if (err) {
