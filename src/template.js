0c2a1672dfff77772397045fbc70e4f717fdfd8a
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-01-15 14:12:11 -0800

    switching to office number instead of phone

--- src/template.js
+++ src/template.js
@@ -44,7 +44,7 @@ const createProfile = team => {
                 <ul class="list-group text-dark">
                     <li class="list-group-item">ID: ${manager.getId()}</li>
                     <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                    <li class="list-group-item">Office number: <a href="tel:${manager.getOfficeNumber()}">${manager.getOfficeNumber()}</a></li>
+                    <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
                 </ul>
             </div>
         </div>
