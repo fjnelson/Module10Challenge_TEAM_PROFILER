8629c8d99c7de226514868888f7f7848fd438854
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-01-15 16:59:57 -0800

    adding tests

---
+++ tests/Manager.test.js
@@ -0,0 +1,25 @@
+const Employee = require('../Develop/lib/employee');
+const Manager = require('../Develop/lib/manager');
+
+test('creates a manager object', () => {
+    const manager = new Manager('Linda');
+});
+
+test('set office number with constructor', () => {
+    const testValue = 100;
+    const e = new Manager('Foo', 1, 'manager@email.com', testValue);
+    expect(e.officeNumber).toBe(testValue);
+});
+
+test('get office number with getOfficeNumber() method', () => {
+    const testValue = 100;
+    const e = new Manager('Foo', 1, 'manager@email.com', testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
+});
+
+// Test if the getRole() value is Manager
+test('getRole() return Manager', () => {
+    const testValue = 'Manager';
+    const e = new Manager('Foo', 1, 'manager@email.com', 100);
+    expect(e.getRole()).toBe(testValue);
+});
